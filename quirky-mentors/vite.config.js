1e29f16f083c60ad2d92821b69f82258e39e4800
Tarun Ramachandran <36789694+Ratchet7x5@users.noreply.github.com>
2023-08-13 13:21:13 +1200

    update vite configs, add vercel json

--- quirky-mentors/vite.config.js
+++ quirky-mentors/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vercel from 'vite-plugin-vercel'
 
 // @Ratchet7x5 integrate a server.js file using the guide below
 //Read this for backend integration
@@ -7,7 +8,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercel()],
   build: {
     // generate manifest.json in outDir
     manifest: true,
