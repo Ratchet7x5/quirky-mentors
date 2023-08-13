dcaeeb1e881900bab887de83f305686e391f3378
Kinzi <100537572+Kinzi-c@users.noreply.github.com>
2023-08-13 12:21:22 +1200

    Add save button functionality

--- quirky-mentors/src/App.jsx
+++ quirky-mentors/src/App.jsx
@@ -1,22 +1,22 @@
-import './App.css'
-import SignUp from './SignUp'
-import Search from './Search'
-import About from './About'
-import ProfileSetup from './ProfileSetup'
-import {Routes, Route} from "react-router-dom"
+import "./App.css";
+import SignUp from "./SignUp";
+import Search from "./Search";
+import About from "./About";
+import ProfileSetup from "./ProfileSetup";
+import { Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 
 function App() {
-
   return (
     <>
       <Routes>
-        <Route path="/" element={<About/>}/>
-        <Route path="signup" element={<SignUp/>}/>
-        <Route path="profile-form" element={<ProfileSetup/>}/>
-        <Route path="search" element={<Search/>}/>
+        <Route path="/" element={<About />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="profile-form" element={<ProfileSetup />} />
+        <Route path="search" element={<Search />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- quirky-mentors/src/ProfileSetup.jsx
+++ quirky-mentors/src/ProfileSetup.jsx
@@ -1,163 +1,244 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import styled from "@emotion/styled";
 import {
   Box,
   Typography,
   Avatar,
   Button,
   TextField,
-  ToggleButtonGroup,
-  ToggleButton,
-  Card,
   Select,
   FormControl,
   InputLabel,
   MenuItem,
-  CardContent,
   CardMedia,
 } from "@mui/material";
 import countries from "i18n-iso-countries";
 import enLocale from "i18n-iso-countries/langs/en.json";
 
+countries.registerLocale(enLocale);
+
+const GENDERS = [
+  { value: "Male", label: "Male" },
+  { value: "Female", label: "Female" },
+  { value: "Non-binary", label: "Non-binary" },
+];
+
+const ROLES = [
+  { value: "Mentor", label: "Mentor" },
+  { value: "Mentee", label: "Mentee" },
+];
+
 function ProfileSetup() {
   const nav = useNavigate();
-  const [alignment, setAlignment] = React.useState("left");
   const [role, setRole] = React.useState("");
   const [gender, setGender] = React.useState("");
   const [selectedCountry, setSelectedCountry] = React.useState("");
 
-  countries.registerLocale(enLocale);
-  const handleRoleChange = (event) => {
-    setRole(event.target.value);
-  };
-
-  const selectCountryHandler = (value) => setSelectedCountry(value);
   const countryObj = countries.getNames("en", { select: "official" });
+  const countryArr = Object.entries(countryObj).map(([key, value]) => ({
+    label: value,
+    value: key,
+  }));
 
-  const countryArr = Object.entries(countryObj).map(([key, value]) => {
-    return {
-      label: value,
-      value: key,
+  const [saving, setSaving] = React.useState(false);
+  const [saveSuccess, setSaveSuccess] = React.useState(null);
+  const [saveError, setSaveError] = React.useState(null);
+
+  const handleSave = async () => {
+    setSaving(true);
+    setSaveSuccess(null);
+    setSaveError(null);
+
+    const profileData = {
+      firstName: document.getElementById("first-name").value,
+      lastName: document.getElementById("last-name").value,
+      email: document.getElementById("email").value,
+      gender,
+      role,
+      institution: document.getElementById("institution").value,
+      country: selectedCountry,
     };
-  });
-  const handleGenderChange = (event) => {
-    setGender(event.target.value);
-  };
 
-  const handleAlignment = (event, newAlignment) => {
-    setAlignment(newAlignment);
+    try {
+      const response = await fetch("/api/profile", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(profileData),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setSaveSuccess(data.message);
+      } else {
+        setSaveError(data.error);
+      }
+    } catch (error) {
+      setSaveError("An unexpected error occurred.");
+    } finally {
+      setSaving(false);
+    }
   };
 
   return (
     <>
-      <Button onClick={() => nav("/search")}>Go to Search</Button>
-      <Button onClick={() => nav("/signup")}>Back</Button>
-      <Box>
-        <Typography
-          variant="h3"
-          gutterBottom
-          align="center
-        "
+      <Box sx={{ position: "relative", textAlign: "center", mb: 3 }}>
+        <Button
+          onClick={() => nav("/signup")}
+          sx={{
+            position: "absolute",
+            left: 0,
+            top: "50%",
+            transform: "translateY(-50%)",
+          }}
         >
+          <CardMedia
+            component="img"
+            sx={{ width: 30 }}
+            image="arrow-left.png"
+            alt="Live from space album cover"
+          />
+        </Button>
+        <Typography variant="h3" gutterBottom>
           Profile
         </Typography>
       </Box>
-      <Box
-        component="form"
-        sx={{
-          "& .MuiTextField-root": { s: 1, m: 2, l: 2 },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Avatar alt="Kinzi Ceolin" sx={{ width: 200, height: 200 }}>
-            KC
-          </Avatar>
-        </Box>
-        <Box>
-          <TextField
+      <Box component="form" noValidate autoComplete="off">
+        <AvatarSection />
+        <InfoFields
+          role={role}
+          setRole={setRole}
+          gender={gender}
+          setGender={setGender}
+          selectedCountry={selectedCountry}
+          setSelectedCountry={setSelectedCountry}
+          countryArr={countryArr}
+        />
+        <Box sx={{ display: "flex", justifyContent: "center", mt: 3 }}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => nav("/search")}
             fullWidth
-            required
-            id="outlined-required"
-            label="First Name"
-            defaultValue="First Name"
-          />
-          <TextField
-            fullWidth
-            required
-            id="outlined-required"
-            label="Last Name"
-            defaultValue="Last Name"
-          />
-          <TextField
-            fullWidth
-            disabled
-            id="outlined-disabled"
-            label="E-mail"
-            defaultValue="E-mail"
-          />
-          <Box sx={{ display: "flex", gap: 2, alignItems: "flex-start" }}>
-            <FormControl sx={{ width: "50%", marginRight: 1 }}>
-              <InputLabel id="gender-select-label">Gender</InputLabel>
-              <Select
-                labelId="gender-select-label"
-                id="gender-select"
-                value={gender}
-                label="Gender"
-                onChange={handleGenderChange}
-              >
-                <MenuItem value={"Male"}>Male</MenuItem>
-                <MenuItem value={"Female"}>Female</MenuItem>
-                <MenuItem value={"Non-binary"}>Non-binary</MenuItem>
-              </Select>
-            </FormControl>
-
-            <FormControl sx={{ width: "50%", marginLeft: 1 }}>
-              <InputLabel id="role-select-label">Role</InputLabel>
-              <Select
-                labelId="role-select-label"
-                id="role-select"
-                value={role}
-                label="Role"
-                onChange={handleRoleChange}
-              >
-                <MenuItem value={"Mentor"}>Mentor</MenuItem>
-                <MenuItem value={"Mentee"}>Mentee</MenuItem>
-              </Select>
-            </FormControl>
-          </Box>
-          <TextField
-            fullWidth
-            required
-            id="outlined-required"
-            label="Institution"
-            defaultValue="Institution"
-          />
-          <FormControl fullWidth sx={{ mb: 2 }}>
-            <InputLabel id="demo-simple-select-label">Countries</InputLabel>
-            <Select
-              value={selectedCountry}
-              onChange={(e) => selectCountryHandler(e.target.value)}
-            >
-              {!!countryArr?.length &&
-                countryArr.map(({ label, value }) => (
-                  <MenuItem key={value} value={value}>
-                    {label}
-                  </MenuItem>
-                ))}
-            </Select>
-          </FormControl>
+          >
+            Save
+          </Button>
         </Box>
       </Box>
     </>
+  );
+}
+
+function AvatarSection() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        mb: 2,
+      }}
+    >
+      <Avatar alt="Kinzi Ceolin" sx={{ width: 130, height: 130 }}>
+        KC
+      </Avatar>
+    </Box>
+  );
+}
+
+function InfoFields({
+  role,
+  setRole,
+  gender,
+  setGender,
+  selectedCountry,
+  setSelectedCountry,
+  countryArr,
+}) {
+  return (
+    <Box>
+      <TextField
+        fullWidth
+        required
+        id="first-name"
+        label="First Name"
+        defaultValue=""
+        sx={{ mb: 2 }}
+      />
+      <TextField
+        fullWidth
+        required
+        id="last-name"
+        label="Last Name"
+        defaultValue=""
+        sx={{ mb: 2 }}
+      />
+      <TextField
+        fullWidth
+        disabled
+        id="email"
+        label="E-mail"
+        defaultValue=""
+        sx={{ mb: 2 }}
+      />
+      <Box sx={{ display: "flex", gap: 2, alignItems: "flex-start", mb: 2 }}>
+        <SelectField
+          id="gender-select"
+          labelId="gender-select-label"
+          label="Gender"
+          value={gender}
+          onChange={setGender}
+          options={GENDERS}
+        />
+        <SelectField
+          id="role-select"
+          labelId="role-select-label"
+          label="Role"
+          value={role}
+          onChange={setRole}
+          options={ROLES}
+        />
+      </Box>
+      <TextField
+        fullWidth
+        required
+        id="institution"
+        label="Institution"
+        defaultValue=""
+        sx={{ mb: 2 }}
+      />
+      <SelectField
+        id="country-select"
+        labelId="country-select-label"
+        label="Country"
+        value={selectedCountry}
+        onChange={setSelectedCountry}
+        options={countryArr}
+      />
+    </Box>
+  );
+}
+
+function SelectField({ id, labelId, label, value, onChange, options }) {
+  return (
+    <FormControl fullWidth sx={{ mb: 2 }}>
+      <InputLabel id={labelId}>{label}</InputLabel>
+      <Select
+        labelId={labelId}
+        id={id}
+        value={value}
+        label={label}
+        onChange={(e) => onChange(e.target.value)}
+      >
+        {options.map(({ value, label }) => (
+          <MenuItem key={value} value={value}>
+            {label}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
   );
 }
 
